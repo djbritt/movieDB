55a3588b7f0d6240e024b623798b84c14017e10e
Daniel Britt <28036018+djbritt@users.noreply.github.com>
2019-04-21 15:57:43 -0700

    Fixed logout process not working correctly

--- calls/logout.php
+++ calls/logout.php
@@ -3,16 +3,5 @@
 session_start();
 session_destroy();
 
-if( !headers_sent() ){
-  header("Location: ../index.php");
-}else{
-  ?>
-  <script type="text/javascript">
-  document.location.href="../index.php";
-  </script>
-  Redirecting to <a href="../index.php">The Homepage..</a>
-  <?php
-}
-die();
-
+header("Location: ../index.php");
 ?>
